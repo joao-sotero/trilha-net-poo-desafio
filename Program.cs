b33942fe31c21baa5462e38a95e3e37c0086f76d
Joao Vitor Sotero Vieira da Silva <joao.vieira.silva@avanade.com>
2024-09-05 21:29:19 -0300

    implementando desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("81999712874", "Lumia 1020", "782347rghidfhdf787878784hhd", 4);
+nokia.Ligar();
+Iphone iphone = new Iphone("81999712834", "Iphone 11 pro", "782347rghidfhdf78dc7878784hhd", 16);
+iphone.Ligar();
+
+nokia.InstalarAplicativo("whatsapp");
+iphone.InstalarAplicativo("whatsapp");
+
+nokia.ReceberLigacao();
+iphone.ReceberLigacao();
